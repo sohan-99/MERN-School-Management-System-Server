7875b26ef5f22a20384ce8d5ef43628831c1c2a8
sohan-99 <sohan75632@gmail.com>
2025-06-06 18:48:34 +0600

    How to do custom validation

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -10,14 +10,30 @@ const userNameSchema = new Schema<IUserName>({
   firstName: {
     type: String,
     required: [true, 'First name is required'],
+    trim: true,
+    maxlength: [20, 'First name must be less than or equal to 20 characters'],
+    minlength: [3, 'First name must be more than or equal to 3 characters'],
+    validate: {
+      validator: function (value) {
+        const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
+        return firstNameStr === value;
+      },
+      message: '{VALUE} must start with an uppercase letter',
+    },
   },
   middleName: {
     type: String,
     required: false,
+    trim: true,
+    maxlength: [20, 'Middle name must be less than or equal to 20 characters'],
+    minlength: [3, 'Middle name must be more than or equal to 3 characters'],
   },
   lastName: {
     type: String,
     required: [true, 'Last name is required'],
+    trim: true,
+    maxlength: [20, 'Last name must be less than or equal to 20 characters'],
+    minlength: [3, 'Last name must be more than or equal to 3 characters'],
   },
 });
 // Define the schema for the Gurdian model
@@ -26,19 +42,55 @@ const GurdianSchema = new Schema<IGurdian>({
   fatherOccupation: {
     type: String,
     required: [true, 'Father occupation is required'],
+    trim: true,
+    maxlength: [
+      20,
+      'Father occupation must be less than or equal to 20 characters',
+    ],
+    minlength: [
+      3,
+      'Father occupation must be more than or equal to 3 characters',
+    ],
   },
   fatherContact: {
     type: String,
     required: [true, 'Father contact is required'],
+    trim: true,
+    maxlength: [
+      15,
+      'Father contact must be less than or equal to 15 characters',
+    ],
+    minlength: [
+      10,
+      'Father contact must be more than or equal to 10 characters',
+    ],
   },
   motherName: { type: String, required: [true, 'Mother name is required'] },
   motherOccupation: {
     type: String,
     required: [true, 'Mother occupation is required'],
+    trim: true,
+    maxlength: [
+      20,
+      'Mother occupation must be less than or equal to 20 characters',
+    ],
+    minlength: [
+      3,
+      'Mother occupation must be more than or equal to 3 characters',
+    ],
   },
   motherContact: {
     type: String,
     required: [true, 'Mother contact is required'],
+    trim: true,
+    maxlength: [
+      15,
+      'Mother contact must be less than or equal to 15 characters',
+    ],
+    minlength: [
+      10,
+      'Mother contact must be more than or equal to 10 characters',
+    ],
   },
 });
 // Define the schema for the localGuardian model
@@ -47,20 +99,50 @@ const localGuardianSchema = new Schema<ILocalGuardian>({
   occupation: {
     type: String,
     required: [true, 'Local guardian occupation is required'],
+    trim: true,
+    maxlength: [
+      20,
+      'Local guardian occupation must be less than or equal to 20 characters',
+    ],
+    minlength: [
+      3,
+      'Local guardian occupation must be more than or equal to 3 characters',
+    ],
   },
   contactNumber: {
     type: String,
     required: [true, 'Local guardian contact number is required'],
+    trim: true,
+    maxlength: [
+      15,
+      'Local guardian contact number must be less than or equal to 15 characters',
+    ],
+    minlength: [
+      10,
+      'Local guardian contact number must be more than or equal to 10 characters',
+    ],
   },
   address: {
     type: String,
     required: [true, 'Local guardian address is required'],
+    trim: true,
+    maxlength: [
+      100,
+      'Local guardian address must be less than or equal to 100 characters',
+    ],
+    minlength: [
+      10,
+      'Local guardian address must be more than or equal to 10 characters',
+    ],
   },
 });
 const StudentSchema = new Schema<IStudent>({
   id: {
     type: String,
     required: [true, 'Student ID is required'],
+    trim: true,
+    maxlength: [20, 'Student ID must be less than or equal to 20 characters'],
+    minlength: [3, 'Student ID must be more than or equal to 3 characters'],
     unique: true,
   },
   name: {
@@ -82,10 +164,12 @@ const StudentSchema = new Schema<IStudent>({
   contactNumber: {
     type: String,
     required: [true, 'Contact number is required'],
+    trim: true,
   },
   emargencyContact: {
     type: String,
     required: [true, 'Emergency contact is required'],
+    trim: true,
   },
   bloodGroup: {
     type: String,
@@ -102,10 +186,12 @@ const StudentSchema = new Schema<IStudent>({
   gurdian: {
     type: GurdianSchema,
     required: true,
+    trim: true,
   },
   localGuardian: {
     type: localGuardianSchema,
     required: true,
+    trim: true,
   },
   profileImg: { type: String, required: false },
   isActive: {
