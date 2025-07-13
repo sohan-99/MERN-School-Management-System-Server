88c1c85654d93f3296b770eb3cf32200e5978dc7
sohan-99 <sohan75632@gmail.com>
2025-07-14 02:38:03 +0600

    bug fixing:Aacording to to semester id and year then provided to student id created modified logic

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -8,12 +8,27 @@ const findLastStudentId = async () => {
     { sort: { createdAt: -1 } },
   ).lean();
   // lastStudent?.id is like '2025010001' (year+code+serial)
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generateStudentId = async (payload: IAcademicSemester) => {
   // first time 0000
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
+
+  const lastStudentId = await findLastStudentId();
+  const lastStudentSemesterCode = lastStudentId?.slice(4, 6); //01
+  const lastStudentYear = lastStudentId?.slice(0, 4); //2025
+  const currentSemesterCode = payload.code; //01
+  const currentYear = payload.year; //2025
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode !== currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6); // 0001
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
   incrementId = `${payload.year}${payload.code}${incrementId}`;
